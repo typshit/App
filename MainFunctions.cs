a0b17d6ec513b4a102db69bce5762fa468ae8a6f
typshit <main.typesh@outlook.com>
2024-08-26 09:44:43 +0000

    z

--- MainFunctions.cs
+++ MainFunctions.cs
@@ -132,5 +132,3 @@ public class VMDetection
         return cpuInfo;
     }
 }
-
-
--- obj/Debug/net8.0/App.AssemblyInfo.cs
+++ obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6207fe9d263522460a63f30620823f26c8fa3ad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
